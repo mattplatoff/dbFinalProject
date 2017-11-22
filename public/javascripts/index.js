7c6d034bd98f588f996d5057462a93bd43f9ee11
mattplatoff <mattplatoff@gmail.com>
2017-11-22 14:58:55 -0500

    added form functionality to registration form

---
+++ public/javascripts/index.js
@@ -0,0 +1,21 @@
+const RegisterUser = document.querySelector('#registrationForm');
+RegisterUser.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const name = RegisterUser.querySelector('#name').value
+    const address = RegisterUser.querySelector('#address').value
+    const email = RegisterUser.querySelector('#email').value
+    const phone = RegisterUser.querySelector('#phone').value
+    post('/registerUser', { name, address, email, phone })
+})
+
+function post (path, data) {
+    console.log("Posting:\npath: "+path +"\n data: "+data+"\n");
+    return window.fetch(path, {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    })
+}
