f362c3f269f96b5fc78a09e3696bcc2445f76e24
mattplatoff <mattplatoff@gmail.com>
2017-11-22 19:04:05 -0500

    post dummy routing for registration

--- app.js
+++ app.js
@@ -25,6 +25,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 app.use('/users', users);
 
+app.post('/registerUser', function(req, res) {
+    console.log("Registration post request:\n name: "
+        +req.body.name+"\n address: "
+        +req.body.address+"\n email: "+
+        req.body.email+ "\n phone: "+req.body.phone);
+});
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
