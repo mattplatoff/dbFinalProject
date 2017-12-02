97956c1a6e2d0c338e890878f3c04b9ed711374a
Stephen Quickel <stephen@quickel.net>
2017-12-02 16:59:24 -0500

    Update Hotel Database Data.sql

--- Hotel Database Data.sql
+++ Hotel Database Data.sql
@@ -1,130 +1,132 @@
 
 
-INSERT INTO Hotel(001, 6095551111, '666 Delafield St.', 'New Brunswick', 'New Jersey', 08888);
+INSERT INTO Hotel
 
-INSERT INTO Hotel(002, 6095552222, '45 Central Ave', 'Pittsburg', 'Pensylvania', 09999);
+ VALUES(001, 6095551111, '666 Delafield St.', 'New Brunswick', 'New Jersey', 08888),
 
-INSERT INTO Hotel(003, 6095553333, '77 Woodbridge', 'San Fransisco', 'California', 01010);
+ VALUES(002, 6095552222, '45 Central Ave', 'Pittsburg', 'Pensylvania', 09999),
 
-INSERT INTO Breakfast(001, 'Continental', 'Waffles', 0);
+ VALUES(003, 6095553333, '77 Woodbridge', 'San Fransisco', 'California', 01010);
 
-INSERT INTO Breakfast(001, 'English', 'Just tea', 1);
+INSERT INTO Breakfast VALUES(001, 'Continental', 'Waffles', 0);
 
-INSERT INTO Breakfast(001, 'Italian', 'Pizza', 2);
+INSERT INTO Breakfast VALUES(001, 'English', 'Just tea', 1);
 
-INSERT INTO Breakfast(001, 'American', 'B-sandy', 3);
+INSERT INTO Breakfast VALUES(001, 'Italian', 'Pizza', 2);
 
-INSERT INTO Breakfast(001, 'French', 'Croissant', 5);
+INSERT INTO Breakfast VALUES(001, 'American', 'B-sandy', 3);
 
-INSERT INTO Breakfast(002, 'Continental', 'Waffles', 0);
+INSERT INTO Breakfast VALUES(001, 'French', 'Croissant', 5);
 
-INSERT INTO Breakfast(002, 'English', 'Just tea', 1);
+INSERT INTO Breakfast VALUES(002, 'Continental', 'Waffles', 0);
 
-INSERT INTO Breakfast(002, 'Italian', 'Pizza', 2);
+INSERT INTO Breakfast VALUES(002, 'English', 'Just tea', 1);
 
-INSERT INTO Breakfast(002, 'American', 'B-sandy', 3);
+INSERT INTO Breakfast VALUES(002, 'Italian', 'Pizza', 2);
 
-INSERT INTO Breakfast(002, 'French', 'Croissant', 5);
+INSERT INTO Breakfast VALUES(002, 'American', 'B-sandy', 3);
 
-INSERT INTO Breakfast(003, 'Continental', 'Waffles', 0);
+INSERT INTO Breakfast VALUES(002, 'French', 'Croissant', 5);
 
-INSERT INTO Breakfast(003, 'English', 'Just tea', 1);
+INSERT INTO Breakfast VALUES(003, 'Continental', 'Waffles', 0);
 
-INSERT INTO Breakfast(003, 'Italian', 'Pizza', 2);
+INSERT INTO Breakfast VALUES(003, 'English', 'Just tea', 1);
 
-INSERT INTO Breakfast(003, 'American', 'B-sandy', 3);
+INSERT INTO Breakfast VALUES(003, 'Italian', 'Pizza', 2);
 
-INSERT INTO Breakfast(003, 'French', 'Croissant', 5);
+INSERT INTO Breakfast VALUES(003, 'American', 'B-sandy', 3);
 
-INSERT INTO Service(001, 'Laundy', 3);
+INSERT INTO Breakfast VALUES(003, 'French', 'Croissant', 5);
 
-INSERT INTO Service(001, 'Airport pick-up', 10);
+INSERT INTO Service VALUES(001, 'Laundy', 3);
 
-INSERT INTO Service(001, 'Airport drop-off', 10);
+INSERT INTO Service VALUES(001, 'Airport pick-up', 10);
 
-INSERT INTO Service(001, 'Parking', 2);
+INSERT INTO Service VALUES(001, 'Airport drop-off', 10);
 
-INSERT INTO Service(001, 'Valet', 5);
+INSERT INTO Service VALUES(001, 'Parking', 2);
 
-INSERT INTO Service(002, 'Laundy', 3);
+INSERT INTO Service VALUES(001, 'Valet', 5);
 
-INSERT INTO Service(002, 'Airport pick-up', 10);
+INSERT INTO Service VALUES(002, 'Laundy', 3);
 
-INSERT INTO Service(002, 'Airport drop-off', 10);
+INSERT INTO Service VALUES(002, 'Airport pick-up', 10);
 
-INSERT INTO Service(002, 'Parking', 2);
+INSERT INTO Service VALUES(002, 'Airport drop-off', 10);
 
-INSERT INTO Service(002, 'Valet', 5);
+INSERT INTO Service VALUES(002, 'Parking', 2);
 
-INSERT INTO Service(003, 'Laundy', 3);
+INSERT INTO Service VALUES(002, 'Valet', 5);
 
-INSERT INTO Service(003, 'Airport pick-up', 10);
+INSERT INTO Service VALUES(003, 'Laundy', 3);
 
-INSERT INTO Service(003, 'Airport drop-off', 10);
+INSERT INTO Service VALUES(003, 'Airport pick-up', 10);
 
-INSERT INTO Service(003, 'Parking', 2);
+INSERT INTO Service VALUES(003, 'Airport drop-off', 10);
 
-INSERT INTO Service(003, 'Valet', 5); 
+INSERT INTO Service VALUES(003, 'Parking', 2);
 
-INSERT INTO 'Room-Has'(01, 100, 2, 1, 'Standard room, dont expect much', 'economy', '2018-09-06 00:00:01', '2018-10-06 23:59:59', 33, 001);
+INSERT INTO Service VALUES(003, 'Valet', 5); 
 
-INSERT INTO 'Room-Has'(02, 100, 2, 1, 'Standard room, dont expect much', 'economy', '2018-09-06 00:00:01', '2018-10-06 23:59:59', 33, 001);
+INSERT INTO 'Room-Has' VALUES(01, 100, 2, 1, 'Standard room, dont expect much', 'economy', '2018-09-06 00:00:01', '2018-10-06 23:59:59', 33, 001);
 
-INSERT INTO 'Room-Has'(03, 100, 2, 1, 'Standard room, dont expect much', 'economy', '2018-09-06 00:00:01', '2018-10-06 23:59:59', 33, 001);
+INSERT INTO 'Room-Has' VALUES(02, 100, 2, 1, 'Standard room, dont expect much', 'economy', '2018-09-06 00:00:01', '2018-10-06 23:59:59', 33, 001);
 
-INSERT INTO 'Room-Has'(04, 100, 2, 1, 'Standard room, dont expect much', 'economy', '2018-09-06 00:00:01', '2018-10-06 23:59:59', 33, 001);
+INSERT INTO 'Room-Has' VALUES(03, 100, 2, 1, 'Standard room, dont expect much', 'economy', '2018-09-06 00:00:01', '2018-10-06 23:59:59', 33, 001);
 
-INSERT INTO 'Room-Has'(05, 100, 2, 1, 'Standard room, dont expect much', 'economy', '2018-09-06 00:00:01', '2018-10-06 23:59:59', 33, 001);
+INSERT INTO 'Room-Has' VALUES(04, 100, 2, 1, 'Standard room, dont expect much', 'economy', '2018-09-06 00:00:01', '2018-10-06 23:59:59', 33, 001);
 
-INSERT INTO 'Room-Has'(10, 200, 4, 2, 'Two whole queen sized beds!', 'double', '2018-09-06 00:00:01', '2018-10-06 23:59:59', 35, 001);
+INSERT INTO 'Room-Has' VALUES(05, 100, 2, 1, 'Standard room, dont expect much', 'economy', '2018-09-06 00:00:01', '2018-10-06 23:59:59', 33, 001);
 
-INSERT INTO 'Room-Has'(11, 200, 4, 2, 'Two whole queen sized beds!', 'double', '2018-09-06 00:00:01', '2018-10-06 23:59:59', 35, 001);
+INSERT INTO 'Room-Has' VALUES(10, 200, 4, 2, 'Two whole queen sized beds!', 'double', '2018-09-06 00:00:01', '2018-10-06 23:59:59', 35, 001);
 
-INSERT INTO 'Room-Has'(12, 200, 4, 2, 'Two whole queen sized beds!', 'double', '2018-09-06 00:00:01', '2018-10-06 23:59:59', 35, 001);
+INSERT INTO 'Room-Has' VALUES(11, 200, 4, 2, 'Two whole queen sized beds!', 'double', '2018-09-06 00:00:01', '2018-10-06 23:59:59', 35, 001);
 
-INSERT INTO 'Room-Has'(13, 999, 6, 2, 'The ultimate in luxury, for high rollers only!', 'suite', '2018-09-06 00:00:01', '2018-10-06 23:59:59', 40, 001);
+INSERT INTO 'Room-Has' VALUES(12, 200, 4, 2, 'Two whole queen sized beds!', 'double', '2018-09-06 00:00:01', '2018-10-06 23:59:59', 35, 001);
 
-INSERT INTO 'Room-Has'(14, 999, 6, 2, 'The ultimate in luxury, for high rollers only!', 'suite', '2018-09-06 00:00:01', '2018-10-06 23:59:59', 40, 001);
+INSERT INTO 'Room-Has' VALUES(13, 999, 6, 2, 'The ultimate in luxury, for high rollers only!', 'suite', '2018-09-06 00:00:01', '2018-10-06 23:59:59', 40, 001);
 
-INSERT INTO 'Room-Has'(01, 100, 2, 1, 'Standard room, dont expect much', 'economy', '2018-09-06 00:00:01', '2018-10-06 23:59:59', 33, 002);
+INSERT INTO 'Room-Has' VALUES(14, 999, 6, 2, 'The ultimate in luxury, for high rollers only!', 'suite', '2018-09-06 00:00:01', '2018-10-06 23:59:59', 40, 001);
 
-INSERT INTO 'Room-Has'(02, 100, 2, 1, 'Standard room, dont expect much', 'economy', '2018-09-06 00:00:01', '2018-10-06 23:59:59', 33, 002);
+INSERT INTO 'Room-Has' VALUES(01, 100, 2, 1, 'Standard room, dont expect much', 'economy', '2018-09-06 00:00:01', '2018-10-06 23:59:59', 33, 002);
 
-INSERT INTO 'Room-Has'(03, 100, 2, 1, 'Standard room, dont expect much', 'economy', '2018-09-06 00:00:01', '2018-10-06 23:59:59', 33, 002);
+INSERT INTO 'Room-Has' VALUES(02, 100, 2, 1, 'Standard room, dont expect much', 'economy', '2018-09-06 00:00:01', '2018-10-06 23:59:59', 33, 002);
 
-INSERT INTO 'Room-Has'(04, 100, 2, 1, 'Standard room, dont expect much', 'economy', '2018-09-06 00:00:01', '2018-10-06 23:59:59', 33, 002);
+INSERT INTO 'Room-Has' VALUES(03, 100, 2, 1, 'Standard room, dont expect much', 'economy', '2018-09-06 00:00:01', '2018-10-06 23:59:59', 33, 002);
 
-INSERT INTO 'Room-Has'(05, 100, 2, 1, 'Standard room, dont expect much', 'economy', '2018-09-06 00:00:01', '2018-10-06 23:59:59', 33, 002);
+INSERT INTO 'Room-Has' VALUES(04, 100, 2, 1, 'Standard room, dont expect much', 'economy', '2018-09-06 00:00:01', '2018-10-06 23:59:59', 33, 002);
 
-INSERT INTO 'Room-Has'(10, 200, 4, 2, 'Two whole queen sized beds!', 'double', '2018-09-06 00:00:01', '2018-10-06 23:59:59', 35, 002);
+INSERT INTO 'Room-Has' VALUES(05, 100, 2, 1, 'Standard room, dont expect much', 'economy', '2018-09-06 00:00:01', '2018-10-06 23:59:59', 33, 002);
 
-INSERT INTO 'Room-Has'(11, 200, 4, 2, 'Two whole queen sized beds!', 'double', '2018-09-06 00:00:01', '2018-10-06 23:59:59', 35, 002);
+INSERT INTO 'Room-Has' VALUES(10, 200, 4, 2, 'Two whole queen sized beds!', 'double', '2018-09-06 00:00:01', '2018-10-06 23:59:59', 35, 002);
 
-INSERT INTO 'Room-Has'(12, 200, 4, 2, 'Two whole queen sized beds!', 'double', '2018-09-06 00:00:01', '2018-10-06 23:59:59', 35, 002);
+INSERT INTO 'Room-Has' VALUES(11, 200, 4, 2, 'Two whole queen sized beds!', 'double', '2018-09-06 00:00:01', '2018-10-06 23:59:59', 35, 002);
 
-INSERT INTO 'Room-Has'(13, 999, 6, 2, 'The ultimate in luxury, for high rollers only!', 'suite', '2018-09-06 00:00:01', '2018-10-06 23:59:59', 40, 002);
+INSERT INTO 'Room-Has' VALUES(12, 200, 4, 2, 'Two whole queen sized beds!', 'double', '2018-09-06 00:00:01', '2018-10-06 23:59:59', 35, 002);
 
-INSERT INTO 'Room-Has'(14, 999, 6, 2, 'The ultimate in luxury, for high rollers only!', 'suite', '2018-09-06 00:00:01', '2018-10-06 23:59:59', 40, 002);
+INSERT INTO 'Room-Has' VALUES(13, 999, 6, 2, 'The ultimate in luxury, for high rollers only!', 'suite', '2018-09-06 00:00:01', '2018-10-06 23:59:59', 40, 002);
 
-INSERT INTO 'Room-Has'(01, 100, 2, 1, 'Standard room, dont expect much', 'economy', '2018-09-06 00:00:01', '2018-10-06 23:59:59', 33, 003);
+INSERT INTO 'Room-Has' VALUES(14, 999, 6, 2, 'The ultimate in luxury, for high rollers only!', 'suite', '2018-09-06 00:00:01', '2018-10-06 23:59:59', 40, 002);
 
-INSERT INTO 'Room-Has'(02, 100, 2, 1, 'Standard room, dont expect much', 'economy', '2018-09-06 00:00:01', '2018-10-06 23:59:59', 33, 003);
+INSERT INTO 'Room-Has' VALUES(01, 100, 2, 1, 'Standard room, dont expect much', 'economy', '2018-09-06 00:00:01', '2018-10-06 23:59:59', 33, 003);
 
-INSERT INTO 'Room-Has'(03, 100, 2, 1, 'Standard room, dont expect much', 'economy', '2018-09-06 00:00:01', '2018-10-06 23:59:59', 33, 003);
+INSERT INTO 'Room-Has' VALUES(02, 100, 2, 1, 'Standard room, dont expect much', 'economy', '2018-09-06 00:00:01', '2018-10-06 23:59:59', 33, 003);
 
-INSERT INTO 'Room-Has'(04, 100, 2, 1, 'Standard room, dont expect much', 'economy', '2018-09-06 00:00:01', '2018-10-06 23:59:59', 33, 003);
+INSERT INTO 'Room-Has' VALUES(03, 100, 2, 1, 'Standard room, dont expect much', 'economy', '2018-09-06 00:00:01', '2018-10-06 23:59:59', 33, 003);
 
-INSERT INTO 'Room-Has'(05, 100, 2, 1, 'Standard room, dont expect much', 'economy', '2018-09-06 00:00:01', '2018-10-06 23:59:59', 33, 003);
+INSERT INTO 'Room-Has' VALUES(04, 100, 2, 1, 'Standard room, dont expect much', 'economy', '2018-09-06 00:00:01', '2018-10-06 23:59:59', 33, 003);
 
-INSERT INTO 'Room-Has'(10, 200, 4, 2, 'Two whole queen sized beds!', 'double', '2018-09-06 00:00:01', '2018-10-06 23:59:59', 35, 003);
+INSERT INTO 'Room-Has' VALUES(05, 100, 2, 1, 'Standard room, dont expect much', 'economy', '2018-09-06 00:00:01', '2018-10-06 23:59:59', 33, 003);
 
-INSERT INTO 'Room-Has'(11, 200, 4, 2, 'Two whole queen sized beds!', 'double', '2018-09-06 00:00:01', '2018-10-06 23:59:59', 35, 003);
+INSERT INTO 'Room-Has' VALUES(10, 200, 4, 2, 'Two whole queen sized beds!', 'double', '2018-09-06 00:00:01', '2018-10-06 23:59:59', 35, 003);
 
-INSERT INTO 'Room-Has'(12, 200, 4, 2, 'Two whole queen sized beds!', 'double', '2018-09-06 00:00:01', '2018-10-06 23:59:59', 35, 003);
+INSERT INTO 'Room-Has' VALUES(11, 200, 4, 2, 'Two whole queen sized beds!', 'double', '2018-09-06 00:00:01', '2018-10-06 23:59:59', 35, 003);
 
-INSERT INTO 'Room-Has'(13, 999, 6, 2, 'The ultimate in luxury, for high rollers only!', 'suite', '2018-09-06 00:00:01', '2018-10-06 23:59:59', 40, 003);
+INSERT INTO 'Room-Has' VALUES(12, 200, 4, 2, 'Two whole queen sized beds!', 'double', '2018-09-06 00:00:01', '2018-10-06 23:59:59', 35, 003);
 
-INSERT INTO 'Room-Has'(14, 999, 6, 2, 'The ultimate in luxury, for high rollers only!', 'suite', '2018-09-06 00:00:01', '2018-10-06 23:59:59', 40, 003);
+INSERT INTO 'Room-Has' VALUES(13, 999, 6, 2, 'The ultimate in luxury, for high rollers only!', 'suite', '2018-09-06 00:00:01', '2018-10-06 23:59:59', 40, 003);
+
+INSERT INTO 'Room-Has' VALUES(14, 999, 6, 2, 'The ultimate in luxury, for high rollers only!', 'suite', '2018-09-06 00:00:01', '2018-10-06 23:59:59', 40, 003);
 
 
 
