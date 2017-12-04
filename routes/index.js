e4110b61520865b24a66248e764e57c4c68202ce
Mohit Mori <mohitmori1997@gmail.com>
2017-12-04 02:06:47 +0000

    checks if email already exists in the db when registerting

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ var mysql = require('mysql');
 var con = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "password",
+  password: "1234",
   database: "hulton_hotels"
 });
 
@@ -69,21 +69,46 @@ function getRoomReviews(result, req, callback){
   });
 }
 
+function checkEmail(data, callback){
+	var emailexists = 0;
+	var query = "SELECT Email FROM Customer"
+	con.query(query, function(err, result){
+		if (err) throw err;
+		result.forEach(function(record, index){
+			if(record['Email']==data.email) emailexists = 1;
+			if(emailexists || result.length - 1 == index) callback(emailexists);
+		});
+	});
+}
+
 function checkValidity(data, callback){
+	//valid = 1
+	//passwords don't match = 0
+	//a field was empty = 2
+	//email already exists in db = 3;
+
 	var valid = 1;
-	if(data.name==""||data.email==""||data.phone==""||data.email==""||data.password==""||data.confpassword=="") valid=0;
-	if(data.password!=data.confpassword) valid=0;
-	callback(valid);
+	if(data.name==""||data.email==""||data.phone==""||data.email==""||data.password==""||data.confpassword=="") valid=2;
+	else if(data.password!=data.confpassword) valid=0;
+	checkEmail(data, function(exists){
+		if(exists) valid = 3;
+		callback(valid);
+	});
 }
 
 function registerUser(data, callback){
 	var query = "INSERT INTO Customer (Name, Address, Phone_no, Email, Password) VALUES ('"+data.name+"','"+data.address+"','"+data.phone+"','"+data.email+"','"+data.password+"');"
 	checkValidity(data, function(valid){
-		if(valid) {con.query(query, function(err, rows){
-			if (err) throw err;
-			console.log("1 Record inserted");
-			callback();
-		});}
+		if(valid==1) {
+			con.query(query, function(err, rows){
+				if (err) throw err;
+				console.log("1 Record inserted");
+				callback();
+			});
+		}
+		else{
+			//callback();
+		}
 	});
 };
 //END REGION CALLBACKS
